623348507d9b8d76206be811e70caa9871a369f2
gersancabo <gersancabo@gmail.com>
2022-05-16 15:52:26 +0100

    removed unused _controller

--- view/happy_neighbor/src/app/community-portal/services/publication.service.ts
+++ view/happy_neighbor/src/app/community-portal/services/publication.service.ts
@@ -9,6 +9,7 @@ export class PublicationService {
 
   private urlPublicationControllerSelectCommunityPublications = ADDRESS_SERVER + "PublicationController/selectCommunityPublications.php";
   private urlPublicationControllerSelect = ADDRESS_SERVER + "PublicationController/select.php";
+  private urlPublicationControllerInsert = ADDRESS_SERVER + "PublicationController/insert.php";
 
   constructor(private httpClient: HttpClient) { }
 
@@ -26,5 +27,14 @@ export class PublicationService {
     formData.append("id_community",idCommunity.toString());
     formData.append("id",idPublication.toString());
     return this.httpClient.post(this.urlPublicationControllerSelect, formData);
+  }
+
+  insertPublication(session_token: string, idCommunity: number, textPublication: string, commentTo: number = 0) {
+    let formData: FormData = new FormData();
+    formData.append("session_token",session_token);
+    formData.append("id_community",idCommunity.toString());
+    formData.append("text_publication",textPublication);
+    formData.append("comment_to",commentTo.toString());
+    return this.httpClient.post(this.urlPublicationControllerInsert, formData);
   }
 }
